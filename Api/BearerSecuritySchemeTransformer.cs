db558917f8bf9f1dd5d5cb19dc556283ff20e581
sarvarbek7 <bakhtiyorov.sarvar2001@gmail.com>
2025-05-14 19:30:36 +0500

    add namespace to BearerSecuritySchemeTransformer

--- Api/BearerSecuritySchemeTransformer.cs
+++ Api/BearerSecuritySchemeTransformer.cs
@@ -1,7 +1,8 @@
-
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.OpenApi;
 using Microsoft.OpenApi.Models;
+
+namespace Api;
 
 internal sealed class BearerSecuritySchemeTransformer(IAuthenticationSchemeProvider authenticationSchemeProvider) : IOpenApiDocumentTransformer
 {
